385cf59552220d5c879c0ce0122f5f21d3b1634a
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-10-26 21:08:23 +0530

    Linking

---
+++ Link of source code in C/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void second(void);
+
+int main()
+{
+    printf("\n This Statement is From the main program ");
+    second();
+
+    return 0;
+}
---
+++ Link of source code in C/second.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void second(void)
+
+{
+    printf("\n This print Statement is from the second program!!!");
+}
