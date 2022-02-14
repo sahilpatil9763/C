1817274a4f8320850d3cc76913115136c3369743
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2022-02-14 23:48:10 +0530

    Calculate Area

---
+++ Practice Questions/02) Calculate Area of Circle & Volume of Cylinder.C
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int r = 3;
+    float pi = 3.14;
+    int height = 3;
+
+    printf("\n The Area of Circle is %f \n", pi*r*r);
+
+    printf("\n Volume of this Cylinder is %f \n", pi*r*r*height);
+
+    getch();
+    return 0;
+}
